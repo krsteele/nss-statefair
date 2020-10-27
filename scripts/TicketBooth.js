66b95d80b7b9cd63765352a74290b9b2d1de5a14
Kristen Steele <ksteelephoto@gmail.com>
2020-10-27 10:49:12 -0500

    add food ticket button to buttons div

--- scripts/TicketBooth.js
+++ scripts/TicketBooth.js
@@ -1,13 +1,21 @@
 const contentTarget = document.querySelector(".entry")
 const eventHub = document.querySelector("#state-fair")
 
-eventHub.addEventListener()
+
 
 export const TicketBooth = () => {
     contentTarget.innerHTML = `
         <div class="ticketBooth">
             <button id="rideTicket">Ride Ticket</button>
+            <button id="foodTicket">Food Ticket</button>
         </div>
     `
 }
 
+eventHub.addEventListener("click", clickEvent => {
+    if (clickEvent.target.id === "rideTicket") {
+        // console.log("button was clicked!")
+        const rideEvent = new CustomEvent("rideTicketPurchased")
+        eventHub.dispatchEvent(rideEvent)
+    }
+})
